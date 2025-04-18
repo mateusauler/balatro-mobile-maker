f0627d120512976209b131f5bc3be8fd554fe560
Mateus Auler <mateus@auler.dev>
2025-04-18 20:08:18 -0300

    devenv

---
+++ devenv.nix
@@ -0,0 +1,12 @@
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}:
+{
+  # https://devenv.sh/languages/
+  languages.dotnet.enable = true;
+
+  # See full reference at https://devenv.sh/reference/options/
+}
